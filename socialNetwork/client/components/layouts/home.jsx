28d19bd7794240d391615e9ad2a198cbc40f382f
Tejas Pancholi <tejas.er@outlook.com>
2016-03-16 09:19:32 +0530

    initial commit

---
+++ socialNetwork/client/components/layouts/home.jsx
@@ -0,0 +1,19 @@
+Homelayout = React.createClass({
+    render(){
+        return (
+            <div>
+                Header
+                <div className="Container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            Features
+                        </div>
+                        <div className="col-md5 col-md-offset-1">
+                            Signup
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+});
